31092f108e971b1ab1db999920d8054e989f0f10
adoth <adoth0929@gmail.com>
2020-08-05 00:44:06 +0900

    Create click listener
    a

--- app/src/main/java/com/example/aiboroid/DevicesFragment.kt
+++ app/src/main/java/com/example/aiboroid/DevicesFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -47,7 +48,12 @@ class DevicesFragment : Fragment() {
         this.viewModel.devices.observe(viewLifecycleOwner, Observer {
             binding.progressBar.visibility = View.INVISIBLE
             binding.deviceRecyclerView.layoutManager = LinearLayoutManager(context)
-            binding.deviceRecyclerView.adapter = CardRecyclerAdapter(this.viewModel.devices.value!!)
+            binding.deviceRecyclerView.adapter = object : CardRecyclerAdapter(this.viewModel.devices.value!!) {
+                @Override
+                override fun onDeviceClicked(deviceId: String) {
+                    Toast.makeText(requireContext(), deviceId, Toast.LENGTH_SHORT).show()
+                }
+            }
         })
 
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
