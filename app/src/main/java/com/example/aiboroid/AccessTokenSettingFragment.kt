a0e77787beefbe02c12d83d80e6ce8f0c79ee950
adoth <adoth0929@gmail.com>
2020-08-05 11:56:38 +0900

    #11 Add paste button (#14)

--- app/src/main/java/com/example/aiboroid/AccessTokenSettingFragment.kt
+++ app/src/main/java/com/example/aiboroid/AccessTokenSettingFragment.kt
@@ -1,10 +1,14 @@
 package com.example.aiboroid
 
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
 import com.example.aiboroid.databinding.FragmentAccessTokenSettingBinding
@@ -33,14 +37,54 @@ class AccessTokenSettingFragment : Fragment() {
         }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun onPasteClipBoardButtonClick() {
+        val clipboard =
+            requireActivity().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        if (!clipboard.hasPrimaryClip()) {
+            return
+        }
+
+        val item = clipboard.primaryClip!!.getItemAt(0)
+        val clipboardText = item.text.toString()
+        if (viewModel.invalidAccessToken(clipboardText)) {
+            showInvalidAccessToken()
+            return
+        }
+        binding.inputAccessToken.setText(clipboardText)
+        binding.accessTokenRegisterButton.isEnabled = true
+    }
+
     fun onResisterButtonClick(accessToken: String) {
+        if (TextUtils.isEmpty(accessToken)) {
+            showNeedAccessTokenToast()
+            return
+        } else if (viewModel.invalidAccessToken(accessToken)) {
+            showInvalidAccessToken()
+            return
+        }
         viewModel.storeAccessToken(accessToken)
         findNavController()
             .navigate(R.id.action_accessTokenSettingFragment_to_devicesFragment)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun showNeedAccessTokenToast() {
+        Toast.makeText(
+            requireContext(),
+            R.string.access_token_toast_empty_access_token,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun showInvalidAccessToken() {
+        Toast.makeText(
+            requireContext(),
+            R.string.access_token_toast_invalid_access_token,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
