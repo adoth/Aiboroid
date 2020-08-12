60419bf47705fb74c42e8805367fec4fc8740d05
adoth <adoth0929@gmail.com>
2020-08-12 20:57:35 +0900

    Add app name

--- app/src/main/java/com/example/aiboroid/MainActivity.kt
+++ app/src/main/java/com/example/aiboroid/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.aiboroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
