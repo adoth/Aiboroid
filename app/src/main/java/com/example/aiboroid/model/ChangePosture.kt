ac2e69eda42a8dd43f820e92d16cea9b8d837bd1
adoth <adoth0929@gmail.com>
2020-08-15 13:22:43 +0900

    To Upper

--- app/src/main/java/com/example/aiboroid/model/ChangePosture.kt
+++ app/src/main/java/com/example/aiboroid/model/ChangePosture.kt
@@ -2,18 +2,18 @@ package com.example.aiboroid.model
 
 class ChangePosture(override val displayName: String, changePostureType: ChangePostureType) : SingleParameter {
     enum class ChangePostureType {
-        back,
-        crouch,
-        down,
-        down_and_lengthen_behind,
-        down_and_shorten_behind,
-        sit_and_raise_both_hands,
-        sit,
-        sleep,
-        stand,
-        stand_straight
+        BACK,
+        CROUCH,
+        DOWN,
+        DOWN_AND_LENGTHEN_BEHIND,
+        DOWN_AND_SHORTEN_BEHIND,
+        SIT_AND_RAISE_BOTH_HANDS,
+        SIT,
+        SLEEP,
+        STAND,
+        STAND_STRAIGHT
     }
 
-    override val parameter: String = changePostureType.toString()
+    override val parameter: String = changePostureType.toString().toLowerCase()
 
 }
